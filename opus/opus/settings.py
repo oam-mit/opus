d345ebcf05f7a213e2c53ab65efbdba3441a2c77
inshamanowar <inshamanowar22@gmail.com>
2020-10-26 14:21:16 +0530

    hopeless opus

--- opus/opus/settings.py
+++ opus/opus/settings.py
@@ -82,18 +82,15 @@ WSGI_APPLICATION = 'opus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'opus_django',
-        'USER':'root',
-        'PASSWORD':'',
-        'HOST':'127.0.0.1',
-        'PORT':'3306',
-    }
+'default':{
+'ENGINE': 'django.db.backends.sqlite3',
+'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 }
-
-
+}
+     
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
--- opus/user/admin.py
+++ opus/user/admin.py
@@ -7,7 +7,7 @@ class ProfileAdmin(admin.ModelAdmin):
     list_display=['name','user','points']
     ordering=['-points']
     search_fields=['user','name']
-    readonly_fields=['reg_number','mob_number']
+    #readonly_fields=['reg_number','mob_number']
 
     def name(self,obj):
         return (obj.user.first_name+' '+obj.user.last_name)
